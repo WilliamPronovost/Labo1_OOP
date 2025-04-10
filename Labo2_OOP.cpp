c76c5bb8e213809edb5c2151016f479de87698e5
William Pronovost <2430066@collegebart.ca>
2025-04-10 12:45:12 -0400

    Achever et compiler Laboratoire 2

--- Labo2_OOP.cpp
+++ Labo2_OOP.cpp
@@ -5,40 +5,45 @@
 
 using namespace std;
 
-void Q1()
-{
-    char word[100];
-    printf("Veuillez entrer votre mot\n");
-    scanf_s("%s", &word);
-    vector<char> letters(26);
-    iota(letters.begin(), letters.end(), 'a');
-    auto it = find(letters.begin(), letters.end(), 'z');
-    for (int i = 0; i < 1; i++) {
-        if (word[i] >= 'a' && word[i] <= 'y') {
-            word[i] = letters[i]++;
-        }
-        else {
-            while (letters[25] <= 'z')
-            {
-                letters[i]++;
-            }
-        }
-        printf("%s", &word[i]);
-    }
-}
-void Q2() {
-    char word[100];
-    printf("Veuillez entrer votre mot\n");
-    scanf_s("%s", &word);
-    vector<char> letters(26);
-    iota(letters.begin(), letters.end(), 'a');
-    auto it = find(letters.begin(), letters.end(), 'z');
-    for (int i = 0; i < letters[0]; i++) {
-        int up = toupper(up);
-    }
-}
+//void Q1()
+//{
+//    char word[100];
+//    printf("Veuillez entrer votre mot\n");
+//    scanf_s("%s", &word);
+//    vector<char> letters(26);
+//    iota(letters.begin(), letters.end(), 'a');
+//    auto it = find(letters.begin(), letters.end(), 'z');
+//    for (int i = 0; word[i] != NULL; i++) {
+//            word[i] = letters[i + 1];
+//        printf("%s", &word[i]);
+//    }
+//}
+//void Q2() {
+//    char word[100];
+//    char up = 'a';
+//    printf("Veuillez entrer votre mot\n");
+//    scanf_s("%s", &word);
+//    vector<char> letters(26);
+//    iota(letters.begin(), letters.end(), 'a');
+//    auto it = find(letters.begin(), letters.end(), 'z');
+//    for (int i = 0; i < letters[0]; i++) {
+//        char up = toupper(up);
+//    }
+//}
 void Q3() {
-
+    const char* word[100];
+    const char* emptySpace = " ";
+    printf("Veuillez entrer vos mots\n");
+    scanf_s("%s", &word);
+    vector<char> letters(26);
+    iota(letters.begin(), letters.end(), 'a');
+    auto it = find(letters.begin(), letters.end(), 'z');
+    for (int i = 0; word != NULL; i++) {
+        int length = strlen(word[i]);
+        if (word[i] = emptySpace) {
+            word[i] = NULL;
+        }
+    }
 }
 void Q4() {
 
@@ -46,10 +51,12 @@ void Q4() {
 void Q5() {
 
 }
-int main(){
+int main() {
     //Q1();
     //Q2();
-    //Q3();
+    Q3();
     //Q4();
     //Q5();
 }
+
+
